c76cca72b0de3df9d0fb814b6f498234769638b4
Katherine <remick.ka@gmail.com>
2022-04-02 15:27:28 -0700

    Some progress with classes

--- src/com/skilldistillery/jets/entities/AirField.java
+++ src/com/skilldistillery/jets/entities/AirField.java
@@ -1,9 +1,30 @@
 package com.skilldistillery.jets.entities;
 
+import java.io.BufferedReader;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
+import java.io.IOException;
 import java.util.List;
 
 public class AirField {
 private List<Jet> fleet;
+
+public AirField(String file) {
+
+	try(BufferedReader br = new BufferedReader(new FileReader(file))){
+		String line = br.readLine();
+		while(line != null) {
+			//Each line is an aircraft - the first string is the type of jet
+		}
+		
+		
+		
+	}catch(FileNotFoundException e) {
+		e.printStackTrace();
+	}catch(IOException e) {
+		e.printStackTrace();
+	}
+}
 
 
 }
--- src/com/skilldistillery/jets/entities/Jet.java
+++ src/com/skilldistillery/jets/entities/Jet.java
@@ -1,5 +1,40 @@
 package com.skilldistillery.jets.entities;
 
 public abstract class Jet {
+	private String model;
+	private int speed;
+	private int range;
+	private double price;
+	
+	public Jet() {}
+	
+	
+	public double getPrice() {
+		return price;
+	}
 
+
+	public void setPrice(double price) {
+		this.price = price;
+	}
+
+
+	public String getModel() {
+		return model;
+	}
+
+
+	public int getSpeed() {
+		return speed;
+	}
+
+
+	public int getRange() {
+		return range;
+	}
+
+
+	public abstract void fly();
+	
+	
 }
